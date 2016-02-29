7fb12f2846e6ba34dcf8a9cd5d27644ef2f44720
Merott Movahedi <merott@merott.com>
2016-02-29 00:06:57 +0000

    chore(karma): add ng2workspace-addon-webpack-karma for testing

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,19 +6,21 @@ import {ng2workspace} from 'ng2workspace';
 (function setupWorkspace() {
   ng2workspace.configure({
     html_title: 'Demo: ng2workspace - AngularClass clone',
+    spec_entry: 'spec.main.js',
     env: {
       hello: 'hola'
     }
   });
 
-  ng2workspace.add('webpack', {}, {webpack: 'bundle'});
+  ng2workspace.add('webpack', {}, { webpack: 'bundle' });
   ng2workspace.add('webpack-assets');
   ng2workspace.add('webpack-index-html');
   ng2workspace.add('webpack-html');
   ng2workspace.add('webpack-css');
   ng2workspace.add('webpack-ts');
-  ng2workspace.add('webpack-tslint', {failOnHint: false});
+  ng2workspace.add('webpack-tslint', { failOnHint: false });
   ng2workspace.add(jsonSupport);
+  ng2workspace.add('webpack-karma', { browsers: ['Chrome'] });
 
   ng2workspace.bootstrap(gulp);
 })();
